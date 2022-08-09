ba955f85b1489a208fe7c84ee655f8f5cde512ea
MUAZU <kakupepe09@gmail.com>
2022-08-09 19:25:40 +0100

    added materialUi for prototyping

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { toast } from 'react-toastify';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
 <h1>React Query</h1>
+<Button variant="contained">Hello World</Button>
     </div>
   );
 }
